4c393c2eb04f7353c4b281f1ffc48d73b774d9fc
murilosantos8908 <murilo.d.araujo@aluno.senai.br>
2026-04-13 08:13:35 -0300

    ..

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.use(express.json());
-
-const userRoutes = require("./routes/users");
-app.use("./users" , userRoutes);
-
-module.exports = app;
