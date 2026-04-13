291fc5dc521ff130e9dca5429d7db21b8637c89b
murilosantos8908 <murilo.d.araujo@aluno.senai.br>
2026-04-13 08:12:06 -0300

    ..

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require("./app");
-const port = process.env.PORT;
-dotenv.config();
-const dotenv = require("dotenv");   
-
-app.listen(port, () => {
-    console.log("servidor rodando em http://localhost:${port}");
-});
